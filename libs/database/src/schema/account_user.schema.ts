c404c9e0de669aba35441790e006d7631842b4e4
neer <neer@email.com>
2026-06-14 17:25:53 +0530

    schema fix

--- libs/database/src/schema/account_user.schema.ts
+++ libs/database/src/schema/account_user.schema.ts
@@ -3,18 +3,18 @@ import { timestamps } from '../lib/timestamps';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { Account } from './account.schema';
-import { User } from './user.schema';
+import { User, UserRole } from './user.schema';
 
-export const UserRoleEnum = {
+export const AccountUserRoleEnum = {
   ADMIN: 'admin',
   MENTOR: 'mentor',
   STUDENT: 'student',
   MANAGER: 'manager',
 } as const;
 
-export const UserRole = pgEnum(
-  'role',
-  Object.values(UserRoleEnum) as [string, ...string[]]
+export const AccountUserRole = pgEnum(
+  'account_role',
+  Object.values(AccountUserRoleEnum) as [string, ...string[]]
 );
 
 export const AccountUser = pgTable('account_user', {
@@ -25,7 +25,7 @@ export const AccountUser = pgTable('account_user', {
   userId: bigserial('user_id', { mode: 'number' })
     .notNull()
     .references(() => User.id),
-  role: UserRole('role').notNull().default(UserRoleEnum.STUDENT),
+  role: AccountUserRole('role').notNull().default(AccountUserRoleEnum.STUDENT),
   isActive: boolean('is_active').default(true).notNull(),
   ...timestamps,
 });
